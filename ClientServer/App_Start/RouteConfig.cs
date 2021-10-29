fb1b53558c16573bea1303df205d23f86a07e659
Nguyen Hoang <hoang.nguyen@fruitful.io>
2021-10-29 18:29:37 +0700

    remove assets add api config

--- ClientServer/App_Start/RouteConfig.cs
+++ ClientServer/App_Start/RouteConfig.cs
@@ -11,12 +11,15 @@ namespace ClientServer
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
+            routes.MapMvcAttributeRoutes();
+
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "ClientServer.Controllers" }
             );
         }
     }
---
+++ ClientServer/Controllers/api/SanPhamController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using ClientServer.Models;
+
+namespace ClientServer.Controllers.api
+{
+    public class SanPhamController : ApiController
+    {
+        private NKSLKDbContext context = new NKSLKDbContext();
+
+        [HttpGet]
+        public async Task<IEnumerable<SanPham>> Get()
+        {
+            List<SanPham> list = await context.SanPhams.ToListAsync();
+            if (list == null)
+            {
+                return new SanPham[] { };
+            }
+            return list;
+        }
+
+        // GET api/<controller>/5
+        public async Task<SanPham> Get(int id)
+        {
+            return await context.SanPhams.FindAsync(id);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
