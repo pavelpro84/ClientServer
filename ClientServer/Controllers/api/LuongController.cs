f05fb800e5c4134486d41402e56dbf5d4ba271cf
pavelpro84@gmail.com <pavelpro84@gmail.com>
2021-11-07 22:46:03 +0700

    first commit

---
+++ ClientServer/Controllers/api/LuongController.cs
@@ -0,0 +1,28 @@
+﻿using ClientServer.Models;
+using ClientServer.Models.QModel;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ClientServer.Controllers.api
+{
+    public class LuongController : ApiController
+    {
+        private NKSLKDbContext context = new NKSLKDbContext();
+        [HttpGet]
+        [Route("api/luong/ngaycong")]
+        public async Task<IEnumerable<NgayCong>> GetSLKThang(string date)
+        {
+            SqlParameter dateParams = new SqlParameter("@Date", date);
+            string query = "EXEC Count_Ngay_Cong @Date";
+
+            var list = await context.Database.SqlQuery<NgayCong>(query, dateParams).ToListAsync();
+            return list;
+        }
+    }
+}
---
+++ ClientServer/Models/QModel/NgayCong.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClientServer.Models.QModel
+{
+    public class NgayCong
+    {
+        public decimal soCong { get; set; }
+        public string hoTen { get; set; }
+    }
+}
